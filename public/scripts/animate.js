d6d3e74350744fdd43d89082a3c14db1b7ed1d90
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-02-17 01:19:50 -0300

    animações

---
+++ public/scripts/animate.js
@@ -0,0 +1,9 @@
+if(document.querySelector('.flashMsg')){
+  setTimeout(()=>{
+    document.querySelector('.flashMsg').classList.add('flashIn')
+  }, 100)
+  setTimeout(()=>{
+    document.querySelector('.flashMsg').classList.remove('flashIn')
+    document.querySelector('.flashMsg').classList.add('flashOut')
+  }, 3000)
+}
