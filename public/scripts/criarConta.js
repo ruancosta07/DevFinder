2de07fa0bca86c6f0a5209b0b96e1359a792229f
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-02-17 01:21:10 -0300

    animacao formulario

---
+++ public/scripts/criarConta.js
@@ -0,0 +1,77 @@
+let password = document.querySelector('input[type="password"]');
+let verSenha = document.getElementById("verSenha");
+let esconderSenha = document.getElementById("esconderSenha");
+let inputName = document.querySelector('input[name="name"]');
+let inputEmail = document.querySelector('input[name="email"]');
+let inputPassword = document.querySelector('input[name="password"]');
+let inputsTexto = document.querySelectorAll('input:not([type="radio"])');
+let labelsInput = document.querySelectorAll(
+  'label:not([for="recrutador"], [for="candidato"])'
+);
+
+inputsTexto.forEach((input) => {
+  input.addEventListener("focusout", function () {
+    if (this.value != "") {
+      this.classList.add("inputAtivo");
+      this.parentElement.querySelector("label").classList.add("labelAtivo");
+    }
+  });
+});
+
+verSenha.addEventListener("click", function () {
+  password.setAttribute("type", "text");
+  this.classList.add("hidden");
+  esconderSenha.classList.remove("hidden");
+});
+
+esconderSenha.addEventListener("click", function () {
+  password.setAttribute("type", "password");
+  this.classList.add("hidden");
+  verSenha.classList.remove("hidden");
+});
+
+if (document.querySelector(".alertName")) {
+  inputName.classList.add("error");
+  inputName.parentElement.querySelector("label").classList.add("errorLabel");
+}
+if (document.querySelector(".alertEmail")) {
+  inputEmail.classList.add("error");
+  inputEmail.parentElement.querySelector("label").classList.add("errorLabel");
+}
+if (document.querySelector(".alertPassword")) {
+  inputPassword.classList.add("error");
+  inputPassword.parentElement.querySelector("label").classList.add("errorLabel");
+}
+
+inputName.addEventListener("focus", function () {
+  if (document.querySelector(".alertName")) {
+    document.querySelector(".alertName").classList.add("hidden");
+    this.parentElement.parentElement.removeChild(
+      this.parentElement.parentElement.querySelector(".alertName")
+    );
+  }
+  inputName.classList.remove("error");
+  inputName.parentElement.querySelector("label").classList.remove("errorLabel");
+});
+
+inputEmail.addEventListener("focus", function () {
+  if (document.querySelector(".alertEmail")) {
+    document.querySelector(".alertEmail").classList.add("hidden");
+    this.parentElement.parentElement.removeChild(
+      this.parentElement.parentElement.querySelector(".alertEmail")
+    );
+  }
+  inputEmail.classList.remove("error");
+  inputEmail.parentElement.querySelector("label").classList.remove("errorLabel");
+});
+
+inputPassword.addEventListener("focus", function () {
+if (document.querySelector(".alertPassword")) {
+    document.querySelector(".alertPassword").classList.add("hidden");
+    this.parentElement.parentElement.removeChild(
+      this.parentElement.parentElement.querySelector(".alertPassword")
+    );
+  }
+  inputPassword.classList.remove("error");
+  inputPassword.parentElement.querySelector("label").classList.remove("errorLabel");
+});
