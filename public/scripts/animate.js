58f805d45b9232c6b361df84159d8ae76961f198
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-02-21 00:10:51 -0300

    animações

--- public/scripts/animate.js
+++ public/scripts/animate.js
@@ -1,9 +1,38 @@
-if(document.querySelector('.flashMsg')){
-  setTimeout(()=>{
-    document.querySelector('.flashMsg').classList.add('flashIn')
-  }, 100)
-  setTimeout(()=>{
-    document.querySelector('.flashMsg').classList.remove('flashIn')
-    document.querySelector('.flashMsg').classList.add('flashOut')
-  }, 3000)
-}
+let elementsAnimateLeft = document.querySelectorAll('[data-animate="left"]');
+let elementsAnimateRight = document.querySelectorAll('[data-animate="right"]');
+let animateVaga = document.querySelectorAll('[data-animate="vaga"]');
+let solucoesLeft = document.querySelector('[data-animate="solucoes-left"]');
+let solucoesRight = document.querySelector('[data-animate="solucoes-right"]');
+
+elementsAnimateLeft.forEach((element, index) => {
+  setTimeout(() => {
+    element.classList.add("animateLeft");
+  }, 300 * index);
+});
+
+elementsAnimateRight.forEach((element, index) => {
+  setTimeout(() => {
+    element.classList.add("animateLeft");
+  }, 300 * index);
+});
+
+window.addEventListener("scroll", () => {
+  let windowTop = window.scrollY + window.innerHeight * 0.95;
+  animateVaga.forEach((vaga, index) => {
+    if (windowTop > vaga.offsetTop) {
+      setTimeout(() => {
+        vaga.classList.add("animateVaga");
+      }, 300 * index);
+    }
+  });
+});
+
+window.addEventListener("scroll", () => {
+  let windowTop = window.scrollY + window.innerHeight * 0.95;
+  if (windowTop > solucoesLeft.offsetTop) {
+    solucoesLeft.classList.add("animateLeft");
+  }
+  if (windowTop > solucoesRight.offsetTop) {
+    solucoesRight.classList.add("animateRight");
+  }
+});
