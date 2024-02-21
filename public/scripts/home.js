7c7f7d32475a2465235e6792b59a8401537a33f8
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-02-21 00:09:10 -0300

    animações

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,10 +1,27 @@
-if(document.querySelector('.account')){
-    document.querySelector('.account').addEventListener('click', ()=>{
-        document.querySelector('.actions-account').classList.toggle('hidden')
-        setTimeout(()=>{
-            document.querySelector('.actions-account').classList.toggle('h-[0px]')
-            document.querySelector('.actions-account').classList.toggle('text-[0rem]')
-            document.querySelector('.actions-account').classList.toggle('text-[.95rem]')
-        }, 1)
-    })
+if (document.querySelector(".account")) {
+  document.querySelector(".account").addEventListener("click", function () {
+    this.querySelector("i").style.transition = ".25s ease";
+    this.querySelector("i").classList.toggle("rotate-180");
+    document.querySelector(".actions-account").classList.toggle("hidden");
+    setTimeout(() => {
+      document.querySelector(".actions-account").classList.toggle("h-[0px]");
+      document
+        .querySelector(".actions-account")
+        .classList.toggle("text-[0rem]");
+      document
+        .querySelector(".actions-account")
+        .classList.toggle("text-[.95rem]");
+    }, 1);
+  });
 }
+
+document.getElementById("menu-mobile").addEventListener("change", function () {
+  this.parentElement.querySelector("ul").style.transition = ".35s ease";
+  this.parentElement.querySelector("ul").style.height = "0px";
+  this.parentElement.querySelector("ul").style.fontSize = "0px";
+  this.parentElement.querySelector("ul").classList.toggle("max-lg:hidden");
+  setTimeout(() => {
+    this.parentElement.querySelector("ul").style.height = "inherit";
+    this.parentElement.querySelector("ul").style.fontSize = "1.2rem";
+  });
+});
--- public/scripts/verVaga.js
+++ public/scripts/verVaga.js
@@ -2,31 +2,68 @@ let vagasCards = document.querySelectorAll(".vagaCard");
 let vagasExpanded = document.querySelectorAll(".vagaExpanded");
 
 function fechaVaga(event) {
-  this.classList.remove("on");
+  document.querySelector(".vaga-ativa").classList.remove("on");
+  document.body.style.overflowY = "scroll";
   vagasExpanded.forEach((item) => {
     item.classList.add("hidden");
   });
+  if (document.querySelector(".filtros")) {
+    document.querySelector(".filtros").classList.toggle("filtrosAnimate");
+    document.querySelector(".filtros").classList.toggle("max-lg:right-full");
+    document.querySelector(".ativa-filtros").classList.toggle("hidden");
+  }
 }
 
-document.querySelector(".vaga-ativa").addEventListener("click", fechaVaga);
+if (document.querySelector(".vaga-ativa")) {
+  document.querySelector(".vaga-ativa").addEventListener("click", fechaVaga);
+}
+
+if (document.querySelector(".filtros")) {
+  document.querySelector(".filtros").style.transition = ".3s !important";
+  document.querySelector(".ativa-filtros").addEventListener("click", () => {
+    document.querySelector(".filtros").classList.toggle("max-lg:right-full");
+    document.querySelector(".filtros").classList.toggle("filtrosAnimate");
+    document.querySelector(".ativa-filtros").classList.toggle("hidden");
+    document.querySelector(".vaga-ativa").classList.toggle("on");
+  });
+}
+
+if (document.querySelector(".fecha-filtros")) {
+  document.querySelector(".fecha-filtros").addEventListener("click", () => {
+    document.querySelector(".filtros").classList.toggle("max-lg:right-full");
+    document.querySelector(".filtros").classList.toggle("filtrosAnimate");
+    document.querySelector(".vaga-ativa").classList.toggle("on");
+    document.querySelector(".ativa-filtros").classList.toggle("hidden");
+  });
+}
 
 function mostraVaga() {
   vagasExpanded.forEach((item) => {
     item.classList.add("hidden");
     if (this.getAttribute("data-id") == item.getAttribute("data-id")) {
+      // document.body.style.overflowY = 'hidden'
       item.classList.add("animate");
-      item.classList.replace("hidden", "fixed");
+      item.classList.replace("hidden", "grid");
       document.querySelector(".vaga-ativa").classList.add("on");
     }
   });
 }
 
+document.querySelectorAll(".fechar-vaga").forEach((close) => {
+  close.addEventListener("click", () => {
+    document.querySelector(".vaga-ativa").classList.remove("on");
+    vagasExpanded.forEach((vaga) => {
+      vaga.classList.add("hidden");
+    });
+  });
+});
+
 vagasCards.forEach((vaga) => {
   vaga.addEventListener("click", mostraVaga);
 });
 
-if (document.querySelector(".searchVaga")) {
-  if (vagasCards.length == 0) {
+if (document.querySelector("h1.searchVaga")) {
+  if (vagasCards.length < 1) {
     document.querySelector(".searchVaga").innerHTML =
       "Oops, parece que busca não retornou nenhum resultado :(";
   }
