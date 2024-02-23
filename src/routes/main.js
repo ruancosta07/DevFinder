7932e45c67871265e66084ad574eb59e16f38426
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-02-22 23:00:41 -0300

    sessão de conta e alteração de senha

--- src/routes/main.js
+++ src/routes/main.js
@@ -9,40 +9,60 @@ const Vaga = mongoose.model("vagas");
 const jwt = require("jsonwebtoken");
 
 main.get("/", (req, res) => {
-  const token = req.cookies.token
-if(token){
-  const decodedToken = jwt.verify(token, `${JWT_KEY}`)
-  Vaga.find()
-    .limit(6)
-    .sort({ date: "desc" })
-    .then((vagas) => {
-      Usuario.findOne({email: decodedToken.email}).then((user)=>{
-        const userEmail = user.email
-        res.render("index", {userEmail, vagas, token});
-      })
-    });
-  }
-  else{
+  const token = req.cookies.token;
+  if (token) {
+    const decodedToken = jwt.verify(token, `${JWT_KEY}`);
     Vaga.find()
-    .limit(6)
-    .sort({ date: "desc" })
-    .then((vagas) => {
+      .limit(6)
+      .sort({ date: "desc" })
+      .then((vagas) => {
+        Usuario.findOne({ email: decodedToken.email }).then((user) => {
+          const userEmail = user.email;
+          res.render("index", { vagas, token, userEmail });
+        });
+      });
+  } else {
+    Vaga.find()
+      .limit(6)
+      .sort({ date: "desc" })
+      .then((vagas) => {
         res.render("index", { vagas, token });
-    });
+      });
   }
 });
 
+main.get("/excluir-vaga/:id", checkRole(), (req, res) => {
+  Vaga.findOneAndDelete({ _id: req.params.id }).then(() => {
+    req.flash("vaga_delete", "Vaga apagada com sucesso!");
+    res.redirect("/");
+  });
+});
 
-main.get('/excluir-vaga/:id', checkRole(), (req, res)=>{
-  Vaga.findOneAndDelete({_id: req.params.id}).then(()=>{
-  req.flash('vaga_delete', 'Vaga apagada com sucesso!')
-  res.redirect('/')
-  })
-})
+main.get("/editar-vaga/:id", checkRole(), (req, res) => {
+  Vaga.findOne({ _id: req.params.id }).then((vaga) => {
+    res.render("editar-vaga", {
+      vaga,
+      level: vaga.level,
+      token: req.cookies.token,
+    });
+  });
+});
 
-// main.get('/excluir-vaga/', checkRole(), (req, res)=>{
-//     res.redirect('/')
-// })
+main.post("/editar-vaga", checkRole(), (req, res) => {
+  let { id, title, enterprise, requirements, salary, level, description } =
+    req.body;
+  let update = { title, enterprise, requirements, salary, level, description };
+
+  Vaga.findOneAndUpdate({ _id: id }, update, { new: true })
+    .then((updatedVaga) => {
+      req.flash("vagaUpdated", "Vaga atualizada com sucesso!");
+      res.redirect(`/editar-vaga/${updatedVaga._id}`);
+    })
+    .catch((err) => {
+      // Trate o erro
+      res.status(500).send(err);
+    });
+});
 
 main.get("/criar-conta", (req, res) => {
   res.render("criar-conta", { token: req.cookies.token });
@@ -111,12 +131,6 @@ main.post("/criar-conta", async (req, res) => {
   }
 });
 
-main.get("/users", (req, res) => {
-  Usuario.find().then((users) => {
-    res.render("users", { users });
-  });
-});
-
 function checkRole() {
   return function (req, res, next) {
     const token = req.cookies.token;
@@ -126,7 +140,7 @@ function checkRole() {
 
     try {
       const decoded = jwt.verify(token, `${JWT_KEY}`);
-      if (decoded.role !== 'recrutador') {
+      if (decoded.role !== "recrutador") {
         return res
           .status(403)
           .send(
@@ -141,11 +155,11 @@ function checkRole() {
 }
 
 main.get("/anunciar-vaga", checkRole(), (req, res) => {
-  const token = req.cookies.token
-  const decodedToken = jwt.verify(token, `${JWT_KEY}`)
-  Usuario.findOne({email: decodedToken.email}).then((user)=>{
-  res.render("anunciar-vaga", { token, user });
-  })
+  const token = req.cookies.token;
+  const decodedToken = jwt.verify(token, `${JWT_KEY}`);
+  Usuario.findOne({ email: decodedToken.email }).then((user) => {
+    res.render("anunciar-vaga", { token, user });
+  });
 });
 
 main.post("/anunciar-vaga", (req, res) => {
@@ -156,7 +170,7 @@ main.post("/anunciar-vaga", (req, res) => {
     salary: req.body.salary,
     level: req.body.level,
     description: req.body.description,
-    createdBy: req.body.createdBy
+    createdBy: req.body.createdBy,
   };
 
   Vaga.create(novaVaga).then(() => {
@@ -194,13 +208,11 @@ main.post("/login", (req, res) => {
               req.flash("password_incorrect", "Senha incorreta");
               return res.redirect("/login");
             }
-            
+
             if (!isMatch) {
               req.flash("password_incorrect", "Senha incorreta");
               return res.redirect("/login");
-            }
-
-            else if (isMatch) {
+            } else if (isMatch) {
               const tokenUser = jwt.sign(
                 { email: user.email, password: user.password, role: user.role },
                 `${JWT_KEY}`,
@@ -212,6 +224,7 @@ main.post("/login", (req, res) => {
                 secure: false,
                 maxAge: 120 * 10 * 1000,
               });
+              req.flash("loginSuccess", "Login efetuado com sucesso");
               res.redirect("/");
             }
           });
@@ -239,19 +252,15 @@ main.get("/logout", function (req, res) {
 main.post("/ver-vagas", async (req, res) => {
   let { searchVaga, min, max, level, front, back, full } = req.body;
 
-
   let query = {};
-
 
   if (searchVaga) {
     query.title = { $regex: new RegExp(searchVaga, "i") };
   }
 
-
   if (min && max) {
     query.salary = { $gte: Number(min), $lte: Number(max) };
   }
-
 
   if (level) {
     query.level = level;
@@ -280,6 +289,76 @@ main.post("/ver-vagas", async (req, res) => {
   });
 });
 
+function userLogged() {
+  return function (req, res, next) {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.redirect("/login");
+    }
+
+    try {
+      const decoded = jwt.verify(token, `${JWT_KEY}`);
+      req.user = decoded;
+      if (
+        req.path === "/minha-conta" &&
+        (decoded.role === "recrutador" || decoded.role === "candidato")
+      ) {
+        return next();
+      }
+      next();
+    } catch (ex) {
+      res.status(400).send("Token inválido.");
+    }
+  };
+}
+
+main.get("/minha-conta", userLogged(), (req, res) => {
+  const token = req.cookies.token;
+  const decodedToken = jwt.verify(token, `${JWT_KEY}`);
+  Usuario.findOne({ email: decodedToken.email }).then((user) => {
+    const { name, email } = user;
+    let match = req.cookies.match;
+    res.render("minha-conta", { name, email, match, token });
+  });
+});
+
+main.post("/minha-conta", userLogged(), async (req, res) => {
+  const token = req.cookies.token;
+  let { name, email, password } = req.body;
+
+  // Agora estamos esperando a promessa ser resolvida antes de continuar
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const update = { name, email, password: hashedPassword };
+  const decodedToken = jwt.verify(token, `${JWT_KEY}`);
+  Usuario.findOneAndUpdate({ email: decodedToken.email }, update, {new: true}).select('+password').then((vagaAtualizada) => {
+    res.clearCookie('token')
+    res.redirect('/minha-conta')
+  });
+});
+
+
+main.post("/minha-conta/alterar-senha", userLogged(), (req, res) => {
+  const { confirmarSenha } = req.body;
+  const token = req.cookies.token;
+  const decodedToken = jwt.verify(token, `${JWT_KEY}`);
+  Usuario.findOne({ email: decodedToken.email })
+    .select("+password")
+    .then((user) => {
+      const userPasswordDB = user.password;
+      bcrypt.compare(confirmarSenha, userPasswordDB, (err, isMatch) => {
+        if (isMatch) {
+          res.cookie("match", "$2a$k960g7R2GWa", { maxAge: 60000 });
+          res.redirect("/minha-conta");
+        }
+        if (!isMatch) {
+          req.flash('change_password_error', 'As senhas não são iguais')
+          res.redirect('/minha-conta')
+        }
+      });
+    });
+});
 
 main.get("/vagas/:id", (req, res) => {
   res.render("/vaga");
