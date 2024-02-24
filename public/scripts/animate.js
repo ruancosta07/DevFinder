b0241ff128dbe1719b571a279dc0030b92241dd8
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-02-24 14:55:00 -0300

    animações gerais

--- public/scripts/animate.js
+++ public/scripts/animate.js
@@ -3,6 +3,7 @@ let elementsAnimateRight = document.querySelectorAll('[data-animate="right"]');
 let animateVaga = document.querySelectorAll('[data-animate="vaga"]');
 let solucoesLeft = document.querySelector('[data-animate="solucoes-left"]');
 let solucoesRight = document.querySelector('[data-animate="solucoes-right"]');
+let animateVagaRender = document.querySelectorAll('[data-animate="vaga-render"]');
 
 elementsAnimateLeft.forEach((element, index) => {
   setTimeout(() => {
@@ -27,12 +28,21 @@ window.addEventListener("scroll", () => {
   });
 });
 
+animateVagaRender.forEach((vaga, index) => {
+  setTimeout(() => {
+    vaga.classList.add("animateVaga");
+  }, 300 * index);
+});
+
 window.addEventListener("scroll", () => {
   let windowTop = window.scrollY + window.innerHeight * 0.95;
+  if(solucoesLeft){
+
   if (windowTop > solucoesLeft.offsetTop) {
     solucoesLeft.classList.add("animateLeft");
   }
   if (windowTop > solucoesRight.offsetTop) {
     solucoesRight.classList.add("animateRight");
+  }
   }
 });
