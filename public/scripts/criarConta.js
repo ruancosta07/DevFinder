2b9ba9d22b4836de37c3db87aa96f05032b7344a
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-02-24 14:56:15 -0300

    respostas visuais ao interagir com o form

--- public/scripts/criarConta.js
+++ public/scripts/criarConta.js
@@ -8,6 +8,7 @@ let inputsTexto = document.querySelectorAll('input:not([type="radio"])');
 let labelsInput = document.querySelectorAll(
   'label:not([for="recrutador"], [for="candidato"])'
 );
+let inputsTipoConta = document.querySelectorAll('.role input[type="radio"]')
 
 inputsTexto.forEach((input) => {
   input.addEventListener("focusout", function () {
@@ -18,23 +19,21 @@ inputsTexto.forEach((input) => {
   });
 });
 
-if(verSenha){
+if (verSenha) {
   verSenha.addEventListener("click", function () {
-  password.setAttribute("type", "text");
-  this.classList.add("hidden");
-  esconderSenha.classList.remove("hidden");
-});
+    password.setAttribute("type", "text");
+    this.classList.add("hidden");
+    esconderSenha.classList.remove("hidden");
+  });
 }
 
-if(esconderSenha){
-esconderSenha.addEventListener("click", function () {
-  password.setAttribute("type", "password");
-  this.classList.add("hidden");
-  verSenha.classList.remove("hidden");
-});
+if (esconderSenha) {
+  esconderSenha.addEventListener("click", function () {
+    password.setAttribute("type", "password");
+    this.classList.add("hidden");
+    verSenha.classList.remove("hidden");
+  });
 }
-
-
 
 if (document.querySelector(".alertName")) {
   inputName.classList.add("error");
@@ -46,23 +45,25 @@ if (document.querySelector(".alertEmail")) {
 }
 if (document.querySelector(".alertPassword")) {
   inputPassword.classList.add("error");
-  inputPassword.parentElement.querySelector("label").classList.add("errorLabel");
+  inputPassword.parentElement
+    .querySelector("label")
+    .classList.add("errorLabel");
 }
 
-if(inputName){
-inputName.addEventListener("focus", function () {
-  if (document.querySelector(".alertName")) {
-    document.querySelector(".alertName").classList.add("hidden");
-    this.parentElement.parentElement.removeChild(
-      this.parentElement.parentElement.querySelector(".alertName")
-    );
-  }
-  inputName.classList.remove("error");
-  inputName.parentElement.querySelector("label").classList.remove("errorLabel");
-});
-
+if (inputName) {
+  inputName.addEventListener("focus", function () {
+    if (document.querySelector(".alertName")) {
+      document.querySelector(".alertName").classList.add("hidden");
+      this.parentElement.parentElement.removeChild(
+        this.parentElement.parentElement.querySelector(".alertName")
+      );
+    }
+    inputName.classList.remove("error");
+    inputName.parentElement
+      .querySelector("label")
+      .classList.remove("errorLabel");
+  });
 }
-
 
 inputEmail.addEventListener("focus", function () {
   if (document.querySelector(".alertEmail")) {
@@ -72,16 +73,34 @@ inputEmail.addEventListener("focus", function () {
     );
   }
   inputEmail.classList.remove("error");
-  inputEmail.parentElement.querySelector("label").classList.remove("errorLabel");
+  inputEmail.parentElement
+    .querySelector("label")
+    .classList.remove("errorLabel");
 });
 
 inputPassword.addEventListener("focus", function () {
-if (document.querySelector(".alertPassword")) {
+  if (document.querySelector(".alertPassword")) {
     document.querySelector(".alertPassword").classList.add("hidden");
-    this.parentElement.parentElement.removeChild(
-      this.parentElement.parentElement.querySelector(".alertPassword")
-    );
   }
   inputPassword.classList.remove("error");
-  inputPassword.parentElement.querySelector("label").classList.remove("errorLabel");
+  inputPassword.parentElement
+    .querySelector("label")
+    .classList.remove("errorLabel");
 });
+
+inputsTexto.forEach((input) => {
+  if (input.value != "") {
+    input.classList.add("inputAtivo");
+    input.classList.remove("error");
+    input.parentElement.querySelector("label").classList.add("labelAtivo");
+    input.parentElement.querySelector("label").classList.remove("errorLabel");
+  }
+});
+
+inputsTipoConta.forEach((input)=>{
+  input.addEventListener('change', ()=>{
+    if(input.parentElement.parentElement.parentElement.querySelector('span.alertPassword')){
+      input.parentElement.parentElement.parentElement.removeChild(input.parentElement.parentElement.parentElement.querySelector('span.alertPassword'))
+    }
+  })
+})
